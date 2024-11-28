aea5f373f605ad0b8951371ecc1aa6b5eed00cad
Raghvendra Pratap <raghvendra@thescalers.com>
2024-11-28 11:36:02 +0530

    Add files via upload

---
+++ AccountDto.java
@@ -0,0 +1,12 @@
+package Raghav.BankingApplication.AccountDto;
+
+import lombok.Data;
+
+@Data
+public class AccountDto {
+
+	
+	private Long id;
+	private String accountHolderName;
+	private double balance;
+}
