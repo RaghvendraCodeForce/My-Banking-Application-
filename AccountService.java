1c2766cd89a7a0c8b63ed4ef14c933ed3badc73f
Raghvendra Pratap <raghvendra@thescalers.com>
2024-11-28 11:29:54 +0530

    Add files via upload

---
+++ AccountService.java
@@ -0,0 +1,8 @@
+package Raghav.BankingApplicaton.service;
+
+import Raghav.BankingApplication.AccountDto.AccountDto;
+
+public interface AccountService {
+
+	AccountDto createAccount(AccountDto accountDto);
+}
